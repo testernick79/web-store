54f8eb5a727e45db891f4c766a337d8759724a61
halfed <1half.ed@gmail.com>
2020-03-17 23:19:49 -0600

    updating package and main.js

--- src/main.js
+++ src/main.js
@@ -1,19 +1,10 @@
 import Vue from 'vue'
-
 import router from './router'
-
 import App from './App.vue'
 
+Vue.config.productionTip = false
 
-/* Vue.use( VueRouter )
-
-const routes = [
-  { path: '/about', component: About },
-  { path: '/', component: Home }
-] */
-
-new Vue( {
-  router: router,
-  el: '#app',
-  render: h => h( App )
-} )
+new Vue({
+    router,
+    render: h => h(App)
+}).$mount('#app')
